e26d2977e9013187f4ae45b8e7df3c792ca2a581
mmsanjana <sanjanabth255@gmail.com>
2026-08-09 16:02:01 +0530

    connect my sql database

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,33 @@
-// අවශ්‍ය දේවල් project එකට සම්බන්ධ කරගැනීම
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2'); // අලුතින් එකතු කරපු පේළිය
 
 const app = express();
 const PORT = 5000;
 
-// මූලික සැකසුම්
 app.use(cors());
 app.use(express.json());
 
-// ඉතා සරල API එකක් (අපේ server එක වැඩද කියලා බලාගන්න)
-app.get('/', (req, res) => {
-    res.send('Waste Management System Backend Running Successfull !');
+// Database එකට සම්බන්ධ වීම (කිසිම සංකීර්ණ security settings නැතුව සරලව)
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // XAMPP වල මුලින්ම password එකක් නැහැ, ඒ නිසා හිස්ව තියන්න
+    database: 'waste_management'
 });
 
-// Server එක පණ ගැන්වීම
+db.connect((err) => {
+    if (err) {
+        console.log('Database cant connect:', err);
+    } else {
+        console.log('Database is connected!');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Waste Management System Backend work  successfully !');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("Server is running on http://localhost:${PORT}");
 });
